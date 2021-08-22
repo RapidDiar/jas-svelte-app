15771ad19a1d5287ab8e082461ab9f51139335a0
Yerkebulan Kenzhebek <yerkebulan@Yerkebulans-MacBook-Pro.local>
2021-08-22 17:21:44 +0600

    pulling from main to dev

--- build/_app/chunks/axiosApi-b32e64fe.js
+++
@@ -1,1 +0,0 @@
-import{V as t}from"./vendor-46274d12.js";const e=t.create({baseURL:"http://127.0.0.1:8000/",timeout:5e3,headers:{"Content-Type":"application/json",accept:"application/json"}});export{e as a};
--- build/_app/chunks/store-185a41c5.js
+++
@@ -1,1 +0,0 @@
-import{C as o}from"./vendor-46274d12.js";const r=o({isLogin:!1,error:!1,metamaskID:""});export{r as a};
